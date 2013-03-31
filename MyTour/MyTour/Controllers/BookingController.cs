c7586e13146f957710fcdf84548c83291eaab018
rajeevkanth <rajeevkanth.m@gmail.com>
2013-03-31 16:07:08 +1000

    places gallery and bookings section

---
+++ MyTour/MyTour/Controllers/BookingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTour.Controllers
+{
+    public class BookingController : Controller
+    {
+        //
+        // GET: /Booking/
+
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView("PartialBooking");
+
+            return View();
+        }
+
+    }
+}
